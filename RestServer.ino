58c63f477f92923ff859ecc461d30374c02f04aa
Enno Herr <enno.herr@outlook.com>
2021-12-28 14:01:05 -0500

    added analog to rest

--- RestServer.ino
+++ RestServer.ino
@@ -1,32 +1,100 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       RestServer.ino
-    Created:	2021-12-28 1:36:05 PM
-    Author:     NOWAY3\ennoh
-*/
+#include <SPI.h>
+#include <Ethernet.h>
+#include <aREST.h>
+#include <avr/wdt.h>
 
-// Define User Types below here or use a .h file
-//
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 2, 20);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress gateway(192, 168, 2, 1);
+IPAddress dns(8, 8, 8, 8);
 
+// Ethernet server
+EthernetServer server(80);
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
+// Create aREST instance
+aREST rest = aREST();
 
+// Variables to be exposed to the API
+int milli;
+float a0 = 0.0;
+float a1 = 0.0;
+float a2 = 0.0;
+float a3 = 0.0;
+float a4 = 0.0;
+float a5 = 0.0;
 
-// Define Functions below here or use other .ino or cpp files
-//
+// Declare functions to be exposed to the API
+int ledControl(String command);
 
-// The setup() function runs once each time the micro-controller starts
-void setup()
+void setup(void)
 {
+	// Start Serial
+	Serial.begin(9600);
 
+	// Init variables and expose them to REST API
+	//milli = millis();
+	//analog0 = analogRead(0);
+	//rest.variable("milli", &milli);
+	rest.variable("a0", &a0);
+	rest.variable("a1", &a1);
+	rest.variable("a2", &a2);
+	rest.variable("a3", &a3);
+	rest.variable("a4", &a4);
+	rest.variable("a5", &a5);
 
+	// Function to be exposed
+	//rest.function("led", ledControl);
+
+	// Give name & ID to the device (ID should be 6 characters long)
+	rest.set_id("008");
+	rest.set_name("dapper_drake");
+
+	// Start the Ethernet connection and the server
+	//if (Ethernet.begin(mac) == 0) {
+	//  Serial.println("Failed to configure Ethernet using DHCP");
+	// no point in carrying on, so do nothing forevermore:
+	// try to congifure using IP address instead of DHCP:
+	Ethernet.begin(mac, ip);
+	//}
+	server.begin();
+	Serial.print("server is at ");
+	Serial.println(Ethernet.localIP());
+
+	// Start watchdog
+	wdt_enable(WDTO_4S);
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
-{
+void loop() {
 
+	//milli = millis();
+	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+	a0 = analogRead(0) * (5.0 / 1023.0);
+	a1 = analogRead(1) * (5.0 / 1023.0);
+	a2 = analogRead(2) * (5.0 / 1023.0);
+	a3 = analogRead(3) * (5.0 / 1023.0);
+	a4 = analogRead(4) * (5.0 / 1023.0);
+	a5 = analogRead(5) * (5.0 / 1023.0);
+
+	// listen for incoming clients
+	EthernetClient client = server.available();
+
+	if (client && client.available()) {
+		rest.handle(client);
+		delay(1);
+		client.stop();
+	}
+
+	wdt_reset();
+}
+
+// Custom function accessible by the API
+int ledControl(String command) {
+
+	// Get state from command
+	int state = command.toInt();
+
+	digitalWrite(6, state);
+	return 1;
 
 }
